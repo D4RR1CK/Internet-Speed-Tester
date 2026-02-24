754a6987a6a04d052e6ceb590d68749e04702db9
Derrick Kalaba <Derrickkalabacc@gmail.com>
2026-02-24 14:33:55 +0200

    Refactor index.php to use Tailwind CSS
    Updated the HTML structure to include Tailwind CSS for styling and improved layout.

--- index.php
+++ index.php
@@ -4,18 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Speed Test</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    
-    <h2>Internet Speed Test</h2>
+<body class="bg-blue-500">
+    <div class="font-serif text-center mt-20">
+        <div class="text-3xl mb-20">
+           <h2>Internet Speed Test</h2>
+        </div>
+        <div class="text-2xl">
+            <div class="mb-20"> 
+                <button onClick="startTest()" class=" border border-black bg-gray rounded-lg">Start Test</button><br/>
+            </div>
+            <div>
+                <p>Ping: <span id="ping">-</span> ms</p>
+                <p>Download: <span id="download">-</span> Mbps</p>
+                <p>Upload: <span id="upload">-</span> Mbps</p>
 
-    <button onClick="startTest()">Start Test</button>
-
-    <p>Ping: <span id="ping">-</span> ms</p>
-    <p>Download: <span id="download">-</span> Mbps</p>
-    <p>Upload: <span id="upload">-</span> Mbps</p>
-
-    <script src="assets/app.js"></script>
-
+                <script src="app.js"></script></div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
